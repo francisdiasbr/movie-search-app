01ead919000528b9a819fea95f405e810bdcbbe9
Francis Dias <francisdiasbr@gmail.com>
2024-07-30 12:11:31 -0300

    Configure React Native Paper

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 import App from './src/app/App';
 
 export default function Main() {
   return (
-    <App />
+    <PaperProvider>
+      <App />
+    </PaperProvider>
   );
 }
 
---
+++ src/app/router/rootRouter.ts
@@ -0,0 +1,7 @@
+import Curatory from '../pages/Curatory';
+import Home from '../pages/Home';
+
+export const unloggedUserRouter = {
+  Curatory,
+  Home
+}
